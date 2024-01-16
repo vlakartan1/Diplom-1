c956698b79bbfaebe0bdc09c715c1fc03fbc1068
DESKTOP-CAQCR66\User <vlakartan@gmail.com>
2024-01-16 18:36:43 +0300

    update BunTest.java

--- src/test/java/praktikum/BunTest.java
+++ src/test/java/praktikum/BunTest.java
@@ -1,22 +1,15 @@
 package praktikum;
 
-import org.junit.Before;
+
 import org.junit.Test;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+
 import static org.junit.Assert.assertEquals;
 
 public class BunTest {
-    private Bun bunMock;
     private Bun bun;
     private final String nameBun = "Булочка";
-    private final float priceBun = 1.20f;
 
-    @Before
-    public void setBunMock() {
-        bunMock = mock(Bun.class);
-    }
 
     @Test
     public void testGetName() {
@@ -26,18 +19,9 @@ public class BunTest {
 
     @Test
     public void testGetPrice() {
+        float priceBun = 1.20f;
         bun = new Bun(nameBun, priceBun);
         assertEquals(priceBun, bun.getPrice(), 0.01f);
-    }
-
-
-    @Test
-    public void testBunWithMock() {
-        when(bunMock.getName()).thenReturn(nameBun);
-        when(bunMock.getPrice()).thenReturn(priceBun);
-
-        assertEquals(nameBun, bunMock.getName());
-        assertEquals(priceBun, bunMock.getPrice(), 0.01f);
     }
 
 }
